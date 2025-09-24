88b5551ad2c9309c5733711e3d9bfa049d3d57da
Yan Yoshino <yanyoshino@gmail.com>
2025-09-23 22:56:59 -0300

    feat(schedule): capture form data and generate date/time with unique ID

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -3,8 +3,13 @@ import { populateHours } from "./format-time";
 
 
 const form = document.querySelector("form");
+const clientName = document.getElementById("tutorName");
+const petName = document.getElementById("petName");
+const phoneNumber = document.getElementById("phone");
+const descriptionInput = document.getElementById("description");
 const selectedDate = document.getElementById("date-schedule");
-const selectedDateHome = document.getElementById("date")
+const selectedDateHome = document.getElementById("date");
+
 
 
 // Formatando a data para o dia de hoje dos inputs
@@ -16,3 +21,50 @@ selectedDateHome.min = inputToday;
 
 // Formatando hora
 populateHours();
+
+
+form.onsubmit = async (event) => {
+    // Previne o carrefamento
+    event.preventDefault();
+
+    try {
+        // Recupera o nome
+        const name = clientName.value.trim();
+        if(!name){
+            return alert("Informe seu nome");
+        }
+        // Recupera o nome do pet
+        const pet = petName.value.trim();
+        if(!pet){
+            return alert("Informe o nome do seu animal");
+        }
+        // Recupera o numero
+        const phone = phoneNumber.value.trim();
+        if(!phone){
+            return alert("Informe seu numero");
+        }
+        // Recupera a descrição
+        const description = descriptionInput.value.trim()
+        if(!description){
+            return alert("Informe o serviço")
+        }
+
+        // Recupera a hora
+        const selectedTime = document.getElementById("time").value;
+        if (!selectedTime) {
+            return alert("Selecione um horário");
+        }
+        // Somente a hora
+        const [hour] = selectedTime.split(":");
+        // Adciona a hora na data
+        const when = dayjs(selectedDate.value).add(hour, "hour");
+        // Cria um id
+        const id = new Date().getTime().toString();
+  
+
+
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento.")
+        console.log(error);
+    }
+}
