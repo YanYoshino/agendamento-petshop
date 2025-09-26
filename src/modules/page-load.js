ce4a600d81b3d6c3a03214a1b6739db04b67a9b5
Yan Yoshino <yanyoshino@gmail.com>
2025-09-26 15:58:25 -0300

    feat: initialize hours and schedules on page load

---
+++ src/modules/page-load.js
@@ -0,0 +1,9 @@
+import { populateHours } from "./form/format-time.js";
+import { schedulesDay, schedulesHome } from "./schedules/load.js";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    populateHours();
+    schedulesDay();
+    schedulesHome();
+});
